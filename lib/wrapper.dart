f1776a32e4ab2fed5e9a6d925cf1036cf0c18fda
jaejae-kim <21600134@handong.edu>
2020-12-21 16:52:48 +0900

    login.dart

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weighit/models/user_info.dart';
+import 'package:weighit/screens/login.dart';
 
 class Wrapper extends StatelessWidget {
   @override
@@ -10,9 +11,7 @@ class Wrapper extends StatelessWidget {
     final user = Provider.of<TheUser>(context);
 
     if (user == null) {
-      return Container(
-        child: Text('Login'),
-      );
+      return LoginPage();
     } else {
       return Container(child: Text('Home'));
     }
