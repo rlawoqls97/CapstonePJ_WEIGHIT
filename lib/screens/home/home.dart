59c7fd95b445500179232fbb0519c1f613d860b7
jaejae-kim <21600134@handong.edu>
2020-12-22 17:05:21 +0900

    homepage

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -16,52 +16,105 @@ class _HomePageState extends State<HomePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.grey[700],
-        currentIndex: _selectedIndex,
-        onTap: (int index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: '홈',
+      bottomNavigationBar: SizedBox(
+        height: size.height * 0.1,
+        child: BottomNavigationBar(
+          backgroundColor: Color(0xffF8F7F7),
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Theme.of(context).accentColor,
+          unselectedItemColor: Color(0xff878787),
+          currentIndex: _selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: '홈',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.camera_alt_outlined),
+              label: '내 몸 어때?',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.accessibility),
+              label: '내 상태는?',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.auto_awesome_mosaic),
+              label: '내 능력은?',
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: size.height * 0.22,
+            padding: EdgeInsets.only(top: size.height * 0.03),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: size.height * 0.02,left: size.width * 0.04),
+                  child: Row(
+                    children: [
+                      Text('헬스마스터님', style: Theme.of(context).textTheme.headline1,),
+                      SizedBox(width: size.width * 0.01,),
+                      Text('20일 째 운동중!', style: Theme.of(context).textTheme.headline2,),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: size.height * 0.06, left: size.width * 0.04),
+                  child: Row(
+                    children: [
+                      Text('루틴', style: Theme.of(context).textTheme.headline6,),
+                      Container(
+                        padding: EdgeInsets.only(left: size.width * 0.47),
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text('새로운 루틴 만들기', style: TextStyle(color: Colors.white),),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(22.0),
+                          ),
+                          color: Theme.of(context).accentColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera_alt_outlined),
-            label: '내 몸 어때?',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.accessibility),
-            label: '내 상태는?',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.auto_awesome_mosaic),
-            label: '내 능력은?',
+          Flexible(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('routine').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                if ((snapshot.hasError) || (snapshot.data == null)) {
+                  return Center(child: Text(snapshot.error.toString()));
+                }
+                final posts = _listTiles(context, snapshot.data.docs) ?? [];
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: posts.length,
+                  itemBuilder: (context, index) {
+                    return _postTile(context, posts[index]);
+                  },
+                );
+              },
+            ),
           ),
         ],
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('routine').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          if ((snapshot.hasError) || (snapshot.data == null)) {
-            return Center(child: Text(snapshot.error.toString()));
-          }
-          final posts = _listTiles(context, snapshot.data.docs) ?? [];
-          return ListView.builder(
-            itemCount: posts.length,
-            itemBuilder: (context, index) {
-              return _postTile(context, posts[index]);
-            },
-          );
-        },
       ),
     );
   }
