68e47f444206e7b75a35507c1648f2a362e8b52c
jaejae-kim <21600134@handong.edu>
2020-12-22 13:24:16 +0900

    no

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -2,8 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weighit/models/user_info.dart';
-import 'package:weighit/screens/login.dart';
-
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
