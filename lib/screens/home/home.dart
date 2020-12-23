c6cc5856599792eaf73746a117e43976ae8ce0ac
jaejae-kim <21600134@handong.edu>
2020-12-23 15:04:09 +0900

    routine plus page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -2,6 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:weighit/models/user_info.dart';
+import 'package:weighit/screens/body_status/status.dart';
+import 'package:weighit/screens/camera/camera.dart';
+import 'package:weighit/screens/home/home_card_tile.dart';
+import 'package:weighit/screens/make_routine.dart';
+import 'package:weighit/screens/test/test.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,20 +15,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
-
+  final List<Widget> _children = [CardTile(), Camera(), Status(), Test()];
   @override
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    final size = MediaQuery
+        .of(context)
+        .size;
     return Scaffold(
       bottomNavigationBar: SizedBox(
         height: size.height * 0.1,
         child: BottomNavigationBar(
           backgroundColor: Color(0xffF8F7F7),
           type: BottomNavigationBarType.fixed,
-          selectedItemColor: Theme.of(context).accentColor,
+          selectedItemColor: Theme
+              .of(context)
+              .accentColor,
           unselectedItemColor: Color(0xff878787),
           currentIndex: _selectedIndex,
           onTap: (int index) {
@@ -51,97 +59,7 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: size.height * 0.22,
-            padding: EdgeInsets.only(top: size.height * 0.03),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.only(top: size.height * 0.02,left: size.width * 0.04),
-                  child: Row(
-                    children: [
-                      Text('헬스마스터님', style: Theme.of(context).textTheme.headline1,),
-                      SizedBox(width: size.width * 0.01,),
-                      Text('20일 째 운동중!', style: Theme.of(context).textTheme.headline2,),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: size.height * 0.06, left: size.width * 0.04),
-                  child: Row(
-                    children: [
-                      Text('루틴', style: Theme.of(context).textTheme.headline6,),
-                      Container(
-                        padding: EdgeInsets.only(left: size.width * 0.47),
-                        child: FlatButton(
-                          onPressed: () {},
-                          child: Text('새로운 루틴 만들기', style: TextStyle(color: Colors.white),),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(22.0),
-                          ),
-                          color: Theme.of(context).accentColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-          Flexible(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance.collection('routine').snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-
-                if ((snapshot.hasError) || (snapshot.data == null)) {
-                  return Center(child: Text(snapshot.error.toString()));
-                }
-                final posts = _listTiles(context, snapshot.data.docs) ?? [];
-                return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: posts.length,
-                  itemBuilder: (context, index) {
-                    return _postTile(context, posts[index]);
-                  },
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  List<TheUser> _listTiles(BuildContext context, List<DocumentSnapshot> snapshot) {
-    if(snapshot == null){
-      return null;
-    } else {
-      return snapshot.map((doc){
-        return TheUser(
-          uid: doc.get('uid') ?? '',
-        );
-      }).toList();
-    }
-  }
-
-  Widget _postTile(BuildContext context, TheUser user){
-    return Padding(
-      padding: EdgeInsets.only(top: 2),
-      child: Card(
-        color: Theme.of(context).primaryColor,
-        child: ListTile(
-          onTap: () {},
-          title: Text(user.uid),
-          subtitle: Text(user.uid, style: TextStyle(color: Colors.white),),
-        ),
-      ),
+      body: _children[_selectedIndex],
     );
   }
 }
---
+++ lib/screens/make_routine.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
+
+
+class Routine extends StatefulWidget {
+  @override
+  _RoutineState createState() => _RoutineState();
+}
+
+class _RoutineState extends State<Routine> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 6,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('새로운 루틴', style: TextStyle(color: Colors.black),),
+            bottom: TabBar(
+              indicator: UnderlineTabIndicator(
+                borderSide: BorderSide(color: Theme.of(context).accentColor),
+              ),
+              labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+              tabs: [
+                Tab(child: Container(
+                  child: Text('가슴', style: TextStyle(color: Colors.black),),
+                ),),
+                Tab(child: Container(
+                  child: Text('어깨', style: TextStyle(color: Colors.black),),
+                ),),
+                Tab(child: Container(
+                  child: Text('팔', style: TextStyle(color: Colors.black),),
+                ),),
+                Tab(child: Container(
+                  child: Text('등', style: TextStyle(color: Colors.black),),
+                ),),
+                Tab(child: Container(
+                  child: Text('복부', style: TextStyle(color: Colors.black),),
+                ),),
+                Tab(child: Container(
+                  child: Text('하체', style: TextStyle(color: Colors.black),),
+                ),),
+              ],
+            ),
+            centerTitle: true,
+            backgroundColor: Color(0xffF8F6F6),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.black,),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Text('가슴'),
+              Text('가슴'),
+              Text('가슴'),
+              Text('가슴'),
+              Text('가슴'),
+              Text('가슴'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
