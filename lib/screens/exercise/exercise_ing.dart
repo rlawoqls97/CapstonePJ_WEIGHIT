6d19d7fe29d16f7f5f62e0eba3bd2bb33cbffd5f
gunyguni <manpkh95@gmail.com>
2021-01-12 17:27:24 +0900

    Actions after timer ver.3 DONE

--- lib/screens/exercise/exercise_ing.dart
+++ lib/screens/exercise/exercise_ing.dart
@@ -23,8 +23,8 @@ class _ExercisingScreenState extends State<ExercisingScreen> {
 
   Timer _timer;
   int _start = 10;
-  int selectedTime = 1;
-
+  int selectedTime = 0;
+  bool isTimerRunning;
   @override
   void initState() {
     //list <UserExercise> 안에 운동이름, 개수, 무개, 세트를 다 가져와야 함
@@ -36,6 +36,7 @@ class _ExercisingScreenState extends State<ExercisingScreen> {
     //카드 클릭을 통해 ui 변화시키는 boolean variable
     isDifferentSet = false;
     isDuringSet = true;
+    isTimerRunning = false;
     super.initState();
   }
 
@@ -47,6 +48,8 @@ class _ExercisingScreenState extends State<ExercisingScreen> {
         if (_start == 0) {
           setState(() {
             timer.cancel();
+            isTimerRunning = false;
+            isDuringSet = true;
           });
         } else {
           setState(() {
@@ -189,7 +192,7 @@ class _ExercisingScreenState extends State<ExercisingScreen> {
             SizedBox(
               height: 5,
             ),
-            isDuringSet ? _setUI(size, _setNo) : _timerUI(size)
+            isDuringSet ? _setUI(size, _setNo) : _timerUI(size, context)
           ],
         ),
       ),
@@ -434,29 +437,61 @@ class _ExercisingScreenState extends State<ExercisingScreen> {
     );
   }
 
-  Widget _timerUI(Size size) {
+  Widget _timerUI(Size size, BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          height: size.height * 0.01,
-        ),
+        // Text('$_start'),
         Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(5)),
           ),
           height: size.height * 0.3,
-          child: CupertinoPicker(
-            // magnification: 1.3,
-            children: [
-              Text('45.00', style: TextStyle(fontSize: size.height * 0.05)),
-              Text('60.00', style: TextStyle(fontSize: size.height * 0.05)),
-              Text('90.00', style: TextStyle(fontSize: size.height * 0.05)),
-            ],
-            itemExtent: size.height * 0.07,
-            looping: false,
-            onSelectedItemChanged: (index) => selectedTime = index,
-          ),
+          child: isTimerRunning
+              ? Center(
+                  child: Text(
+                  '$_start',
+                  style: TextStyle(fontSize: size.height * 0.05),
+                ))
+              : CupertinoPicker(
+                  // magnification: 1.3,
+                  children: [
+                    Text('45.00',
+                        style: TextStyle(fontSize: size.height * 0.05)),
+                    Text('60.00',
+                        style: TextStyle(fontSize: size.height * 0.05)),
+                    Text('90.00',
+                        style: TextStyle(fontSize: size.height * 0.05)),
+                  ],
+                  itemExtent: size.height * 0.07,
+                  looping: false,
+                  onSelectedItemChanged: (index) => selectedTime = index,
+                ),
         ),
+        FlatButton(
+          color: Theme.of(context).accentColor,
+          child: Text(
+            '쉬기',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {
+            setState(() {
+              startTimer();
+              isTimerRunning = !isTimerRunning;
+              switch (selectedTime) {
+                case 0:
+                  _start = 1;
+                  break;
+                case 1:
+                  _start = 2;
+                  break;
+                case 2:
+                  _start = 3;
+                  break;
+              }
+            });
+          },
+        ),
+        Text('주어진 시간 동안 휴식하세요'),
       ],
     );
   }
